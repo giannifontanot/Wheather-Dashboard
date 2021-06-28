ec3007dd1cce28365fd915420d44d60efb1630d0
giannifontanot <giannifontanot@gmail.com>
2021-06-27 19:25:17 -0500

    File commented to prevent autocomplete.

--- Assets/scripts/weather-dashboard-jquery.js
+++ Assets/scripts/weather-dashboard-jquery.js
@@ -1,9 +1,9 @@
-    $(document).ready(function(){
-
-
-           // $('#input-city')autocomplete({source: ["uno", "dos", "tres"]});
-
-        $( "#input_city" ).autocomplete({
-          source: [ "Austin", "Rockwall", "Houston", "New York", "San Antonio", "Dallas", "Galveston" ]
-        });
-   });
+   //  $(document).ready(function(){
+   //
+   //
+   //         // $('#input-city')autocomplete({source: ["uno", "dos", "tres"]});
+   //
+   //      $( "#input_city" ).autocomplete({
+   //        source: [ "Austin", "Rockwall", "Houston", "New York", "San Antonio", "Dallas", "Galveston" ]
+   //      });
+   // });
